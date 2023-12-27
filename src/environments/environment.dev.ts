3b0099f2ebfc6da168eb405b6f90805ccf007c72
Swapnil Opash <sra.opash@gmail.com>
2023-12-27 19:37:33 +0530

    initial commit

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,15 @@
+const url = 'https://api.therapist.tube';
+const webUrl = 'https://therapist.tube/';
+const tubeUrl = 'https://video.therapist.tube/'
+// const url = 'http://localhost:8080';
+// const webUrl = 'http://localhost:4200/';
+
+export const environment = {
+  production: false,
+  hmr: false,
+  serverUrl: `${url}/api/v1/`,
+  socketUrl: `${url}/`,
+  webUrl: webUrl,
+  tubeUrl: tubeUrl,
+  domain: '.therapist.tube'
+};
